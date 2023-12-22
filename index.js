7cb7201aee4db5ab526250c354223c92815668c6
cesar <cg.gtav@gmail.com>
2023-12-22 12:19:19 +0100

    counter javascript no working

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function incrementar() {
+
+let numero = parseInt (document.getElementById("counter").textContent);
+
+while(numero < 0){
+    numero++
+    console.log(numero)
+}
+    counterValue.innerHTML = counter;
+
+}
+
+
+function decrementar() {
+    
+}
